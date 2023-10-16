fa1ef9d71f17c3b85e0e25cea3e3e941540dd897
Humbert <humbertgim@gmail.com>
2023-10-16 10:50:13 +0200

    First MVP with blade offset calibration.

--- src/MMT-01.ino
+++ src/MMT-01.ino
@@ -20,6 +20,8 @@
 TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
 
 unsigned long targetTime = 0;
+unsigned long tPushOK = 0;
+int iLengthStateChange = 2000;
 byte red = 31;
 byte green = 0;
 byte blue = 0;
@@ -29,7 +31,11 @@ uint32_t runing = 0;
 
 #define MM 1
 #define CM 10
+#define INCH 2.54
 
+#define STATE_NORMAL 1
+#define STATE_CALIBRATION 2
+int iState = 1;
 
 int unit = CM;
 int CPR = 2400;
@@ -38,10 +44,13 @@ float radius = 25;
 float perimeter = 2*pi*radius;
 int precision = 3;  // Number of digits after decimal point
 
+float fBladeOffset = 0.5; //Blade offset in the selected units
 
 
 int iCountPulses = 0;
 
+bool isBladeOffsetNeeded = false;
+bool isBOKReleased = true;
 
 
 #define Encoder_output_A 13 // pin 12 of the Arduino
@@ -72,51 +81,140 @@ void setup(void)
         case CM:
           precision = 2;
           break;
+        
   }
 
-  theEncoder.setup(Encoder_output_A, Encoder_output_B);
+  theEncoder.setup(Encoder_output_B, Encoder_output_A);
 
 }
 
 void loop()
 {
 
+    //TODO: we need to account for the blade offset
+    //TODO: We need to show 
+    
     
     if (targetTime < millis()) {
         targetTime = millis() + 100;
+        String sUnits = "";
 
+        switch (unit) {
+          case MM:
+            sUnits = " mm";
+            break;
+          case CM:
+            sUnits = " cm";
+            break;
+        }
+        //Get the encoder value at the very beginning
+        float fValue = 0.0;
+        int xpos = 80;
+        
+        fValue = theEncoder.getEncoderCount() * (perimeter / CPR);
+
+        //Button logic for main menu
         if (!digitalRead(BUTTON_PIN_RST)){
-          //iCountPulses = 0;
+          //Paint area when button pressed, and reset the counter
           theEncoder.setEncoderCount(0);
           tft.setTextColor(TFT_WHITE, TFT_BLACK, true);
           tft.drawRoundRect(0, 0, 70, 85, 5, TFT_RED);
           tft.drawString("RST",10,30,4);
         } else{
-          tft.setTextColor(TFT_BLACK, TFT_BLACK, true);
-          tft.drawRoundRect(0, 0, 70, 85, 5, TFT_BLACK);
-          tft.drawString("RST",10,30,4);
+          //Clean area when not pressed
+          tft.fillRoundRect(0, 0, 70, 85, 5, TFT_BLACK);
+          
         }
 
-        tft.setTextColor(TFT_WHITE, TFT_BLACK, true);
+        if (!digitalRead(BUTTON_PIN_OK)){
+          //Switch between blade offset mode and normal mode
+          tft.setTextColor(TFT_WHITE, TFT_BLACK, true);
+          if(isBOKReleased){ //Only do it once
+            isBOKReleased = false;
+            tPushOK = millis();
+              if(iState == STATE_NORMAL){
+                isBladeOffsetNeeded = !isBladeOffsetNeeded;
+                if(isBladeOffsetNeeded){
+                  tft.drawString(" Blade offset activated: " + String(fBladeOffset) + sUnits,80,150,2);
+                }else{
+                  tft.drawString(" No blade offset                      " ,80,150,2);  
+                }
+              }else if(iState == STATE_CALIBRATION){
+                fBladeOffset = fValue;
+                iState = STATE_NORMAL;
+                tft.drawString(" Blade offset activated: " + String(fBladeOffset) + sUnits,80,150,2);
+                isBladeOffsetNeeded = true;
+              }
+          }
+          
+          tft.setTextColor(TFT_WHITE, TFT_BLACK, true);
+          tft.drawString(" Maintain pushed to calibrate blade",20,5,2);
+          tft.drawRoundRect(0, 85, 70, 85, 5, TFT_RED);
 
-        float fValue = 0.0;
-        int xpos = 80;
-        fValue = theEncoder.getEncoderCount() * (perimeter / CPR);
+          //Logic to switch to calibration blade mode after the defined time in iLengthStateChange
+          //Also paint a progress bar to indicate the user that certain time has passed
+          unsigned long now = millis();
+          float percent = 0.0f;
+          if(now - tPushOK < iLengthStateChange){
+            percent = abs((float)(now - tPushOK) / (float)iLengthStateChange);
+          }else{
+            percent = 1.0f;
+            iState = STATE_CALIBRATION;
+            isBladeOffsetNeeded = false;
+            tft.setTextColor(TFT_WHITE, TFT_BLACK, true);
+            tft.drawString("Calibrating blade                 ",80,150,2);
+          }
+          int step = (int)(85.0*percent);
+          if(step < 10){
+            step = 10; //This solves a bug in the rendering of the roundrect (it is discounting the radius, so it paints in negative areas!!!!)
+          }
+          Serial.println("Percent: " + String(percent) + " Step: " + String(step));
+          tft.fillRoundRect(0,85,70, step,5,TFT_RED);
+          tft.setTextColor(TFT_WHITE, TFT_BLACK, false);
+
+          tft.drawString("BO",10,115,4);
+        } else{
+          isBOKReleased = true;
+          tft.setTextColor(TFT_BLACK, TFT_BLACK, true);
+          tft.drawString(" Maintain pushed to calibrate blade",20,5,2);
+          tft.fillRoundRect(0, 85, 70, 170, 5, TFT_BLACK);
+          //tft.drawString("BO",10,115,4);
+        }
+
+        
+
+        //Draw the value taking into account the blade offset option
+        tft.setTextColor(TFT_WHITE, TFT_BLACK, true);
+        if(isBladeOffsetNeeded){
+          if(fValue < fBladeOffset){
+            tft.setTextColor(TFT_RED, TFT_BLACK, true);
+            fValue = 0.0;
+          }else{
+            fValue = fValue - fBladeOffset;
+          }
+        }
         xpos += tft.drawFloat(fValue, precision, xpos, 54, 6);
         tft.drawString("                ", xpos, 54, 6);
-        String s = "";
-
-        switch (unit) {
-          case MM:
-            s = " mm";
-            break;
-          case CM:
-            s = " cm";
-            break;
-        }
-        xpos += tft.drawString(s + "                   ", xpos, 70, 4);
+        
+        xpos += tft.drawString(sUnits + "                   ", xpos, 70, 4);
 
         tft.drawRoundRect(0, 0, 320, 170, 5, TFT_RED);
+
+
+        //Draw the circle with the moving line
+        int iRadius = 20;
+        tft.fillCircle(290,30,iRadius,TFT_WHITE);
+        tft.drawCircle(290,30,iRadius,TFT_BLUE);
+        int x = 0;
+        int y = 0;
+        int counter = theEncoder.getEncoderCount();
+        
+        float fAngle = (((float)counter / (float)CPR)*360.0f) * (pi/-180.0f);
+        x = (int)(sinf(fAngle) * (float)iRadius);
+        y = (int)(cosf(fAngle) * (float)iRadius)*-1; //The -1 is to make it go in the right direction with the wheel
+        //Serial.println("Counter: " + String(counter) + " fAngle: " + String(fAngle) + " x,y: " + String(x) + "," + String(y));
+        tft.drawLine(290,30,290 + x,30 + y,TFT_RED);
+        
 
         
 
